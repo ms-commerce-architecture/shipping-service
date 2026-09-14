8dbffbbc60b31020964bb2d4a5437d9043884ae7
BelAnouar <anwarbelhassan@example.com>
2026-09-14 19:48:49 +0100

    feat(kafka): add OrderEventConsumer background service for order-event topic

---
+++ Kafka/OrderCreatedEvent.cs
@@ -0,0 +1,8 @@
+﻿namespace shipping_service_backend.Kafka
+{
+    public class OrderCreatedEvent
+    {
+        public string OrderNumber { get; set; }= string.Empty;
+        public string Email { get; set; }= string.Empty;
+    }
+}
